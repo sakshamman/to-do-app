6bb258474e310e89ccc72476315828d7d78613d2
Saksham Man Shrestha <sakshammanshrestha@gmail.com>
2024-09-03 13:49:24 +0545

    initial comit

---
+++ src/ToDoList.jsx
@@ -0,0 +1,73 @@
+import React, {useState} from 'react'
+
+function ToDoList(){
+
+    const [tasks, setTasks] = useState(["Eat Breakfast", "Take a shower", "Walk the dog"]);
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChange(event){
+        setNewTask(event.target.value);
+    }
+
+    function addTask(){
+
+    }
+
+    function deleteTask(index){
+
+    }
+
+    function moveTaksUp(index){
+
+    }
+
+    return(
+    <div className ="to-do-list">
+
+        <h1>To-Do-List</h1>
+    
+        <div>
+            <input
+                type="text"
+                placeholder='Enter a task...'
+                value={newTask}
+                onChange={handleInputChange}/>
+            <button 
+                className="add-button"
+                onClick={addTask}>
+                Add Task
+            </button>
+        </div>
+
+        <ol>
+            {tasks.map((task, index) =>
+                <li key={index}>
+                    <span className='text'>{task}</span>
+                    <button
+                        className="delete-button"
+                        onClick={()=> deleteTask(index)}>
+                            Delete
+                    </button>
+                    <button
+                        className="move-button"
+                        onClick={()=> moveTaksUp(index)}>
+                            Up
+                    </button>
+                    <button
+                        className="move-button"
+                        onClick={()=> moveTaksDown(index)}>
+                            Down
+                    </button>
+                </li>
+            
+            )}
+        </ol>
+
+
+
+    </div>    
+    );
+
+}
+
+export default ToDoList
